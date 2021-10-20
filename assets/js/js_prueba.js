cb88be6e78a295f87b27811b54630b5566ef48fd
xKamui86 <91355406+xKamui86@users.noreply.github.com>
2021-10-19 22:23:34 -0500

    Add files via upload

---
+++ assets/js/js_prueba.js
@@ -0,0 +1,20 @@
+//Funcion de validacion de evento
+
+function setData_New_User(e){
+    
+    var objname = document.getElementById("name_user");
+    var objemail = document.getElementById("email");
+    var objpassword = document.getElementById("password");
+    var objcc = document.getElementById("cc");
+    var objphone = document.getElementById("phone");
+    alert("si funciona");
+    console.log(objcc);
+    console.log(objemail);
+    console.log(objname);
+    console.log(objpassword);
+    console.log(objphone);
+
+    debugger;
+    e.preventDefault();
+}
+  
